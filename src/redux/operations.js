e2bca3c994d8459ab63f35ea71dcd13bedb91539
Arthur Korostelev <korostelev.art.ua@gmail.com>
2023-04-18 17:36:42 +0300

    Update operations.js

--- src/redux/operations.js
+++ src/redux/operations.js
@@ -1,7 +1,7 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://635679c2a2d1844a9775a1d4.mockapi.io';
+axios.defaults.baseURL = 'https://643a8c7c90cd4ba563fbeae2.mockapi.io';
 
 export const fetchContacts = createAsyncThunk(
   'contacts/fetchAll',
